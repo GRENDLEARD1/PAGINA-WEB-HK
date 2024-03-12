c9b4ef660c47f60d2c47615772ff0803a666f833
David Triana <davidtriana796@gmail.com>
2024-03-12 14:10:55 -0500

    hash

--- nodes.mjs
+++ nodes.mjs
@@ -10,14 +10,6 @@ export const services = document.querySelector('.services')
 export const home = document.querySelector('.home');
 export const notices = document.querySelector('.notices');
 export const contact = document.querySelector('.contact');
-<<<<<<< HEAD
-
-
-
-
-
 
 export const containerServices = document.querySelector('.container-services');
-=======
 export const containerHome = document.querySelector('.container-home');
->>>>>>> eb5c3b61b6c3b3fd3e52342e0858f26b55d5088e
