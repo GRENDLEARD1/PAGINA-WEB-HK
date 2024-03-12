b570a014f6e30be77780f3c271587a29b6c946fe
David Triana <davidtriana796@gmail.com>
2024-03-12 12:35:56 -0500

    hash

--- navigation.mjs
+++ navigation.mjs
@@ -8,15 +8,46 @@ window.addEventListener('hashchange',navigate)
 function navigate(){
     if(location.hash.startsWith('#bienvenido')){
         bienvenido()
-
+    } else if(location.hash.startsWith('#home')){
+        homeSection()
+    } else if(location.hash.startsWith('#services')){
+        serviceSection()
+    } else if(location.hash.startsWith('#notices')){
+        noticeSection()
+    }else if(location.hash.startsWith('#contact')){
+        contactSection()
     }
+
+
 }
 
 function bienvenido(){
-    btnp.style.display = 'none'
-    registro.style.display = 'none'
+    btnp.classList.add('inactive')
+    registro.classList.add('inactive')
     services.classList.remove('inactive')
     home.classList.remove('inactive')
     notices.classList.remove('inactive')
     contact.classList.remove('inactive')
+}
+
+function homeSection(){
+    console.log('home');
+    btnp.classList.add('inactive')
+    services.classList.remove('inactive')
+    home.classList.remove('inactive')
+    notices.classList.remove('inactive')
+    contact.classList.remove('inactive')
+    
+}
+
+function serviceSection(){
+    console.log('services');
+}
+
+function noticeSection(){
+    console.log('notices');
+}
+
+function contactSection(){
+    console.log('contact');
 }
